a60e357e6b780b5e5e1ca90e28d4628e436aa0b9
neoncd <neoncd@fixelsoft.com>
2021-08-13 14:19:41 +0900

    firsr Commit

---
+++ src/main/java/com/fix/mymain/MymainApplication.java
@@ -0,0 +1,13 @@
+package com.fix.mymain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fix/mymain/MymainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fix.mymain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
